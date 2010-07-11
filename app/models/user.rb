cf4069ef750e33e7f29c2c8d0b0d0f067c95a5cb
Simon Fenton <swfenton@gmail.com>
2010-07-11 20:47:56 +1000

    Part way through adding password functionality to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -19,4 +20,30 @@ class User < ActiveRecord::Base
   validates_length_of     :name, :maximum => 50
   validates_format_of     :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+  
+  # Automatically create the virtual attribute 'password_confirmation'
+  validates_confirmation_of :password
+  
+  # Password validations
+  validates_presence_of   :password
+  validates_length_of     :password, :within => 6..40
+  
+  before_save   :encrypt_password
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
+  end
+  
+  private
+  
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      string
+    end
+  
 end
